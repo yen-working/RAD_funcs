826c3b7ce50ecd31787e3187b43c34d57eb88cd9
yen-working <178304218+yen-working@users.noreply.github.com>
2025-03-18 21:26:05 +0000

    Lint and format

--- Database/Metric/Metric.py
+++ Database/Metric/Metric.py
@@ -12,48 +12,51 @@ Currently implemented fields:
 from typing import Literal
 
 ################################################################################
-actions = Literal['SUM', 'DISTINCT', 'RATIO']
-fields = Literal['patients', 'studies']
+actions = Literal["SUM", "DISTINCT", "RATIO"]
+fields = Literal["patients", "studies"]
 field_to_table = {
-   'patients': ('patients', 'patient_name'),
-   'studies': ('studies', 'study_uid')
+    "patients": ("patients", "patient_name"),
+    "studies": ("studies", "study_uid"),
 }
 
+
 class Metric:
-   # --------------------------------------------------------------------------
-   def __init__(self, action: actions, first_field: fields, second_field: fields = ''):
-      self.action = self.validate_metric(action, first_field, second_field)
-      self.first_field = first_field
-      self.second_field = second_field
-      self.sql = self.get_sql()
+    # --------------------------------------------------------------------------
+    def __init__(self, action: actions, first_field: fields, second_field: fields = ""):
+        self.action = self.validate_metric(action, first_field, second_field)
+        self.first_field = first_field
+        self.second_field = second_field
+        self.sql = self.get_sql()
 
-   # --------------------------------------------------------------------------
-   def validate_metric(self, action, first_field, second_field):
-      if first_field == '':
-         raise ValueError('Field cannot be empty.')
-      if action == 'RATIO' and second_field == '':
-         raise ValueError('Second value cannot be empty when the action is RATIO.')
-      if action != 'RATIO' and second_field != '':
-         raise ValueError(f'Second field cannot be non-empty if the action is {action}.')
-      return action   
-    
-   # --------------------------------------------------------------------------
-   def get_sql(self):
-      action = self.action
-      first_field = self.first_field
-      first_field_table = field_to_table[first_field][0]
-      first_field_column = field_to_table[first_field][1]
+    # --------------------------------------------------------------------------
+    def validate_metric(self, action, first_field, second_field):
+        if first_field == "":
+            raise ValueError("Field cannot be empty.")
+        if action == "RATIO" and second_field == "":
+            raise ValueError("Second value cannot be empty when the action is RATIO.")
+        if action != "RATIO" and second_field != "":
+            raise ValueError(
+                f"Second field cannot be non-empty if the action is {action}."
+            )
+        return action
 
-      second_field = self.second_field
+    # --------------------------------------------------------------------------
+    def get_sql(self):
+        action = self.action
+        first_field = self.first_field
+        first_field_table = field_to_table[first_field][0]
+        first_field_column = field_to_table[first_field][1]
 
-      if action == 'DISTINCT':
-         return f'SELECT COUNT(DISTINCT {first_field_column}) FROM {first_field_table}'
-      
-      if action == 'RATIO':
-         second_field_table = field_to_table[second_field][0]
-         second_field_column = field_to_table[second_field][1]
-         return f'''SELECT 
+        second_field = self.second_field
+
+        if action == "DISTINCT":
+            return (
+                f"SELECT COUNT(DISTINCT {first_field_column}) FROM {first_field_table}"
+            )
+
+        if action == "RATIO":
+            second_field_table = field_to_table[second_field][0]
+            second_field_column = field_to_table[second_field][1]
+            return f"""SELECT 
          (SELECT COUNT(DISTINCT {first_field_column}) FROM {first_field_table})
-         (SELECT COUNT(DISTINCT {second_field_column}) FROM {second_field_table})'''
-
-    
+         (SELECT COUNT(DISTINCT {second_field_column}) FROM {second_field_table})"""
